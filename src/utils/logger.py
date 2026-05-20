9358028d9f357b5591e378ab649229aa2f461109
Renan Toyoyama <renan.toyoyama@sefa.pa.gov.br>
2026-05-20 20:22:57 -0300

    feat: adiciona logger centralizado

---
+++ src/utils/logger.py
@@ -0,0 +1,25 @@
+import logging
+from string import Formatter
+import sys
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
