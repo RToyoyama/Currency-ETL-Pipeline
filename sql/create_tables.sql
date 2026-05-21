835b88dff7ac9d4284d9bb90405d391dd0316368
Renan Toyoyama <renan.toyoyama@sefa.pa.gov.br>
2026-05-21 17:04:58 -0300

    feat: adiciona script SQL de criação da tabela

---
+++ sql/create_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS currency_rates (
+    id              SERIAL PRIMARY KEY,
+    currency        VARCHAR(20)     NOT NULL,
+    value           NUMERIC(10,2)   NOT NULL,
+    collected_at    TIMESTAMP       NOT NULL
+):
