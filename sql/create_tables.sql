20d1c5d635b943e26b288a03da8545a07272ab47
Renan Toyoyama <renan.toyoyama@sefa.pa.gov.br>
2026-05-21 21:03:54 -0300

    fix: corrige bugs no XCom da DAG e na chamada table_exists

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS currency_rates (
     currency        VARCHAR(20)     NOT NULL,
     value           NUMERIC(10,2)   NOT NULL,
     collected_at    TIMESTAMP       NOT NULL
-):
+);
